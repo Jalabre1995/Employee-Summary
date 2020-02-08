df1aeac9f7abe285b84bb0e72001f13f54225901
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-02-07 23:12:44 -0500

    Employee-Summary-v3

--- output/output/employee.js
+++ output/output/employee.js
@@ -1,45 +1,79 @@
-const inquirer = require('inquirer')
 
+    const inquirer = require('inquirer')
 
-class Employee {
-    constructor(name, id, title,) {
-        this.name = name;
-        this.id = id;
-        this.title = title;
-        
-/// Name for the Employee////
-const name = "Alice";
-const Employee = new Employee(name);
-        
-    }
-}
-    //////Questions to ask the user////
-
+    // //////Questions to ask the user////
+  
     inquirer.prompt([{
-
-        type: "input",
-        name: "idNumber",
-        message: "id number?"
-    },
-    {
-        type: "input",
-        name: "emailProvided",
-        message: "Enter your eamil",
-    },
-    {
-        type: "input",
-        name: "officeProvided",
-        message: "What is your office number?",
-    }
-    ])
-.then(function(answers){
-    idNumber = answers.idNumber;
-    emailProvided = answers.emailProvided;
-    officeProvided = answers.officeProvided;
-})
-
     
-    ///instantute the object/////
-    getName()
+    type: "input",
+    name: "name",
+    message: "What is your name?"
+  },
+  {
+      type: "input",
+      id: "idNumber",
+      message: "What is your id number?"
+  },
+  {
+      type: "input",
+      email: "emailProvided",
+      message: "What is your eamil",
+  },
+  {
+      type: "input",
+      office: "officeProvided",
+      message: "What is your office number?",
+  }
+  ])
+  .then(function(answers){
+      name = answers.name;
+      idNumber = answers.idNumber;
+      emailProvided = answers.emailProvided;
+     officeProvided = answers.officeProvided;
+  })
+  
+  
+  class Employee {
+      constructor(name, id, title,) {
+          this.name = name;
+          this.id = id;
+          this.title = title;
+   getName(function(){
+      return `Hi ${this.name}`
+  })
+  
+  
+  ////set the id /////
+   getId(function() {
+       const id = 100;
+       const employee2 = new Employee("Alice", id);
+       expect(employee2.id) = (id);
+   });
+  
+   ///set the email//// 
+   getEmail  (function() {
+       const email = "";
+       const employee3 = new Employee('Alice', 1 , email);
+       (employee3.email) = email 
+   });
+  
+   getName(function() {
+     
+      
+  
+   })
+          
+      }
+  }
+  
+  //  Name for the Employee////
+  const employee1 = new Employee("Alice");
+  console.log(employee1);
+  
     
+  
+     
+    
+  
+  
 
